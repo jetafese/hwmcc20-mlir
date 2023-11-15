0f7d0644af840ba9f96a8e01b965e1e057c8aced
Joseph Tafese <jetafese@uwaterloo.ca>
2023-11-15 15:05:18 -0500

    feat: generate btor witness from llvm witness

--- runbench/get_cex_seahorn.sh
+++ runbench/get_cex_seahorn.sh
@@ -23,8 +23,10 @@ echo "btor2mlir-translate --mlir-to-llvmir $1.mlir.opt > $1.mlir.opt.ll" \
 
 # exe-cex
 # --oll=$1.mlir.opt.ll.final.ll
-time timeout 300 sea yama -y ~/hwmc20-mlir/configs/sea-cex.yaml bpf  --verbose=2 --cex=/tmp/h2.ll --log=cex -m64 --keep-temps --temp-dir=/tmp/cex $1.mlir.opt.ll -o$1.mlir.opt.ll.smt2 --bound=20
-echo "time timeout 300 sea yama -y ~/hwmc20-mlir/configs/sea-cex.yaml bpf  --verbose=2 --cex=/tmp/h2.ll --log=cex -m64 --keep-temps --temp-dir=/tmp/cex $1.mlir.opt.ll -o$1.mlir.opt.ll.smt2 --bound=20"
+# echo "time timeout 300 sea yama -y ~/hwmc20-mlir/configs/sea-cex.yaml bpf  --verbose=2 --cex=/tmp/h2.ll --log=cex -m64 --keep-temps --temp-dir=/tmp/cex $1.mlir.opt.ll -o$1.mlir.opt.ll.smt2 --bound=20"
+# time timeout 300 sea yama -y ~/hwmc20-mlir/configs/sea-cex.yaml bpf  --verbose=2 --cex=/tmp/h2.ll --log=cex -m64 --keep-temps --temp-dir=/tmp/cex $1.mlir.opt.ll -o$1.mlir.opt.ll.smt2 --bound=20
+echo "time timeout 300 sea yama -y ~/hwmc20-mlir/configs/sea-vac.yaml bpf  --vac --verbose=2 --cex=/tmp/h2.ll --log=cex -m64 --keep-temps --temp-dir=/tmp/cex $1.mlir.opt.ll -o$1.mlir.opt.ll.smt2 --bound=20"
+time timeout 300 sea yama -y ~/hwmc20-mlir/configs/sea-vac.yaml bpf  --vac --verbose=2 --cex=/tmp/h2.ll --log=cex -m64 --keep-temps --temp-dir=/tmp/cex $1.mlir.opt.ll -o$1.mlir.opt.ll.smt2 --bound=20
 
 clang++-14 $1.mlir.opt.ll /tmp/h2.ll ~/seahorn/build/run/lib/libsea-rt.a -o h2.out 
 echo "clang++-14 $1.mlir.opt.ll /tmp/h2.ll ~/seahorn/build/run/lib/libsea-rt.a -o h2.out"
